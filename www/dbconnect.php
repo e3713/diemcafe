1ab8e5a1e9e4535df0ed0c30f85137dddce89fc4
Adam Newby <adam@adam-debian.studio3713.com>
2017-09-02 18:13:31 +0100

    Force results character set in MySQL to be UTF8. Otherwise, on live system, results may not be returned in UTF8, causing htmlentities() to barf and return an empty string.

--- www/dbconnect.php
+++ www/dbconnect.php
@@ -1,3 +1,5 @@
 <?php
 $dbh = new PDO("mysql:host=localhost;dbname=diem_cafe", "diem_cafe", "");
+// Force results character set. Otherwise, on live system, results may not be returned in UTF8, causing htmlentities() to barf and return an empty string.
+$dbh->query('set character_set_results = "utf8mb4"');
 ?>
